dc11f519caafd4512da0e43646170b6ef25dda0e
Michal Podhradsky <mpodhradsky@galois.com>
2019-01-17 15:21:54 -0800

    Add Picollo demo

---
+++ FreeRTOS/Demo/RISC-V_Bluespec_Picollo/bsp/bsp.h
@@ -0,0 +1,7 @@
+#ifndef RISCV_BLUESPEC_BSP_H
+#define RISCV_BLUESPEC_BSP_H
+
+// Define a temporary external interrupt handler
+void external_interrupt_handler( uint32_t cause ) {}
+
+#endif /* RISCV_BLUESPEC_BSP_H */
