310a2b096cb5208f6420016353b6f97c94e85096
podhrmic @ FPGA2 <mpodhradsky@galois.com>
2019-02-09 10:39:55 -0800

    Fix timer to match CLINT frequency (temporary) and add trace facility

--- FreeRTOS/Demo/RISC-V_Bluespec_Picollo/FreeRTOSConfig.h
+++ FreeRTOS/Demo/RISC-V_Bluespec_Picollo/FreeRTOSConfig.h
@@ -87,13 +87,13 @@
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				1
-#define configCPU_CLOCK_HZ				( ( uint32_t ) ( 10000000 ) )
-#define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
+#define configCPU_CLOCK_HZ				( ( uint32_t ) ( 830000 ) )
+#define configTICK_RATE_HZ				( ( TickType_t ) 100 )
 #define configMAX_PRIORITIES			( 5 )
 #define configMINIMAL_STACK_SIZE		( ( uint32_t ) 512 ) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 280 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 16 )
-#define configUSE_TRACE_FACILITY		0
+#define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			0
 #define configUSE_MUTEXES				1
