8f739e6e05f80e7fab98de4d86932587fa4f5e9f
Michal Podhradsky <mpodhradsky@galois.com>
2019-02-11 12:12:28 -0800

    Blinky test works

--- FreeRTOS/Demo/RISC-V_Bluespec_Picollo/FreeRTOSConfig.h
+++ FreeRTOS/Demo/RISC-V_Bluespec_Picollo/FreeRTOSConfig.h
@@ -87,8 +87,8 @@
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				1
-#define configCPU_CLOCK_HZ				( ( uint32_t ) ( 830000 ) )
-#define configTICK_RATE_HZ				( ( TickType_t ) 100 )
+#define configCPU_CLOCK_HZ				( ( uint32_t ) ( 83000000 ) )
+#define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
 #define configMINIMAL_STACK_SIZE		( ( uint32_t ) 512 ) /* Can be as low as 60 but some of the demo tasks that use this constant require it to be higher. */
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 280 * 1024 ) )
